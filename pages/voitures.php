9e75e0ae98bba4a72a39c09bedf339ddcf5d2224
salma hamdi <saalmahm51@gmail.com>
2024-12-09 15:55:25 +0100

    Affichage des donner de base de donner dans la page voiture

--- pages/voitures.php
+++ pages/voitures.php
@@ -7,12 +7,17 @@
  
  try {
      $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     echo "KHadaaaaaaaaaaaaam";
+    //  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  } catch (PDOException $e) {
      echo "Erreur de connexion : " . $e->getMessage();
      
  }
+$sql = "SELECT * FROM Voitures";
+$stmt = $pdo->prepare($sql);  
+$stmt->execute();  
+
+$voitures = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
  ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -78,8 +83,6 @@
     </div>
 </section>
 
-
-
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-2 my-8">
     <table class="w-full text-sm text-left text-gray-400">
         <thead class="text-xs uppercase bg-gray-50 bg-gray-700 text-gray-400">
@@ -101,84 +104,28 @@
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                <td class="px-6 py-4">
-                    Silver
-                </td>
-                <td class="px-6 py-4">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    $2999
-                </td>
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Microsoft Surface Pro
-                </th>
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="px-6 py-4">
-                    Laptop PC
-                </td>
-                <td class="px-6 py-4">
-                    $1999
-                </td>
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
-            <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Magic Mouse 2
-                </th>
-                <td class="px-6 py-4">
-                    Black
-                </td>
-                <td class="px-6 py-4">
-                    Accessories
-                </td>
-                <td class="px-6 py-4">
-                    $99
-                </td>
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
-        </tbody>
+<?php foreach ($voitures as $voiture): ?> 
+    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+        <?php echo ($voiture['NumImmatriculation']); ?>
+        </th>
+        <td class="px-6 py-4">
+        <?php echo ($voiture['Marque']); ?>
+        </td>
+        <td class="px-6 py-4">
+        <?php echo ($voiture['Modele']); ?>
+        </td>
+        <td class="px-6 py-4">
+        <?php echo ($voiture['Annee']); ?>
+        </td>
+        <td class="px-6 py-4 text-right">
+            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+        </td>
+    </tr>
+<?php endforeach; ?>
+</tbody>
+
     </table>
 </div>
-
-    <!-- <footer class="bg-gray-900 p-8 mt-2">
-        <div class="flex items-center justify-between">
-            <a href="" class="">
-                <img src="/images/cars.gif" class="h-8" alt="Flowbite Logo" />
-            </a>
-            <ul class="flex items-center text-sm font-medium text-gray-400 mb-0 ">
-                <li>
-                    <a href="/pages/clients.php" class="hover:underline me-4 md:me-6">Clients</a>
-                </li>
-                <li>
-                    <a href="/pages/contrats.php" class="hover:underline me-4 md:me-6">Contras</a>
-                </li>
-            </ul>
-        </div>
-        <hr class="my-6 border-gray-200 border-gray-700 " />
-        <span class="block text-sm text-center text-gray-400">© 2024 <a href="https://flowbite.com/"
-                class="hover:underline">Car Rental™</a>. All Rights Reserved.</span>
-    </footer> -->
-
-
-
-
-
 </body>
-
 </html>
