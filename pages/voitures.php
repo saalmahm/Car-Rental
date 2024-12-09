dd3b315ab0449706efa0f3478a7076811a8d01a7
salma hamdi <saalmahm51@gmail.com>
2024-12-09 16:52:46 +0100

    Ajouet un formulaire pour l'ajout du voiture

--- pages/voitures.php
+++ pages/voitures.php
@@ -58,29 +58,42 @@ $voitures = $stmt->fetchAll(PDO::FETCH_ASSOC);
             </ul>
         </div>
     </header>
-    <script>
-        const menu = document.getElementById("burger-icon");
-        const sidebar = document.getElementById("sidebar");
-        const closeSidebar = document.getElementById("close-sidebar");
-
-        menu.addEventListener("click", () => {
-            sidebar.classList.remove("translate-x-full");  
-            sidebar.classList.add("translate-x-0");
-        });
-
-        closeSidebar.addEventListener("click", () => {
-            sidebar.classList.add("translate-x-full");   
-            sidebar.classList.remove("translate-x-0");    
-        });
-    </script>
+   
 
 <section class="bg-blue-200 py-8 relative">
     <div class="px-6 flex items-center justify-between">
         <h1 class="text-4xl sm:text-5xl font-bold text-gray-800 mb-0">List of cars</h1>
-        <button class="bg-blue-600 text-white py-3 px-3 rounded-full font-semibold text-lg hover:bg-blue-700 transition-colors duration-300">
+        <button id="addCar" class="bg-blue-600 text-white py-3 px-3 rounded-full font-semibold text-lg hover:bg-blue-700 transition-colors duration-300">
             Add a new car
         </button>
     </div>
+    
+
+    <div id="modalAdd" class="hidden fixed inset-0 bg-gray-900 bg-opacity-60 flex items-center justify-center z-50">
+  <form class="max-w-sm mx-auto bg-white p-10 rounded-lg">
+    <div class="mb-2">
+      <label for="Resistration " class="block mb-2 text-sm font-medium ">Resistration number</label>
+      <input type="text" id="Resistration" class="border bg-gray-200 p-2 rounded-md" required />
+    </div>
+    <div class="mb-5">
+    <label for="Marque " class="block mb-2 text-sm font-medium ">brand</label>
+    <input type="text" id="Marque" class="border bg-gray-200 p-2 rounded-md" required />
+    </div>
+    <div class="mb-5">
+    <label for="model " class="block mb-2 text-sm font-medium ">model</label>
+    <input type="text" id="model" class="border bg-gray-200 p-2 rounded-md" required />
+    </div>
+    <div class="mb-5">
+    <label for="year " class="block mb-2 text-sm font-medium ">year</label>
+    <input type="number" id="year" class="border bg-gray-200 p-2 rounded-md" required />
+    </div>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</button>
+
+  </form>
+</div>
+
+
 </section>
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-2 my-8">
@@ -100,6 +113,8 @@ $voitures = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 </th>
                 <th scope="col" class="px-6 py-3">
                     <span class="sr-only">Edit</span>
+                    <span class="sr-only">Delete</span>
+
                 </th>
             </tr>
         </thead>
@@ -119,7 +134,9 @@ $voitures = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <?php echo ($voiture['Annee']); ?>
         </td>
         <td class="px-6 py-4 text-right">
-            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit  </a>
+            <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
+
         </td>
     </tr>
 <?php endforeach; ?>
@@ -127,5 +144,26 @@ $voitures = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     </table>
 </div>
+<script>
+        const menu = document.getElementById("burger-icon");
+        const sidebar = document.getElementById("sidebar");
+        const closeSidebar = document.getElementById("close-sidebar");
+        const addCar=document.getElementById("addCar");
+        const addmodal =document.getElementById("modalAdd");
+
+        menu.addEventListener("click", () => {
+            sidebar.classList.remove("translate-x-full");  
+            sidebar.classList.add("translate-x-0");
+        });
+
+        closeSidebar.addEventListener("click", () => {
+            sidebar.classList.add("translate-x-full");   
+            sidebar.classList.remove("translate-x-0");    
+        });
+
+        addCar.addEventListener("click",()=>{
+          addmodal.classList.toggle("hidden");
+        })
+    </script>
 </body>
 </html>
